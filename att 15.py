b18deb43a769f05948a00c219dc8d3664ccbcfb0
mariaeryka <erykamariab4@gmail.com>
2024-09-11 15:35:31 -0300

    operacao

---
+++ att 15.py
@@ -0,0 +1,7 @@
+# Crie um programa que receba dois números e uma operação (soma, subtração, multiplicação ou divisão) e execute a operação desejada.
+
+num1 = float(input("Digite o primeiro número: "))
+num2 = float(input("Digite o segundo número: "))
+
+resultado = num1 + num2
+print("Resultado:", resultado)
